a0622793c6eca22dd4b4a3162c919f4139eca11b
KYRIAKOS MARAGKOS <maragkos.kyriakos00@gmail.com>
2025-12-09 10:52:42 +0000

    > Build failed because of webpack errors /layers/google.nodejs.firebasen

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  basePath: '/studio',
   typescript: {
     ignoreBuildErrors: true,
   },
